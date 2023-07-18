6084f2059b8e2c5be6ee04bb27f4e82b28ef035e
azizios011 <hdriaziz699@gmail.com>
2023-07-18 08:42:41 -1100

    j

--- models/base_model.py
+++ models/base_model.py
@@ -3,10 +3,19 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())  
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            # If kwargs is not empty, re-create the instance from the dictionary representation
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            # If kwargs is empty, create a new instance
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
