50d59a1e29508c87272afb30aef2c9e3ac3f5bff
azizios011 <hdriaziz699@gmail.com>
2023-07-18 08:29:06 -1100

    h

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+# models/__init__.py
+
+# This file is intentionally left empty.
+# It serves as an indicator that the 'models' directory is a package.
+# Any initialization code or imports can be added here if needed.
+# For this specific project, no extra initialization is required in this file.
