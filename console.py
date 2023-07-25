4da801223b842a9b93bf5996d4a4650bd0674bb5
azizios011 <hdriaziz699@gmail.com>
2023-07-25 04:47:03 -1100

    Console 0.0.1

--- console.py
+++ console.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python3
 import cmd
-import models
+from models.base_model import BaseModel
+
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
-    
+
     def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
 
     def do_EOF(self, arg):
-        """Exit the program on EOF (Ctrl + D)"""
+        """Exit the program gracefully"""
         print()
         return True
 
     def emptyline(self):
-        """Override emptyline method to do nothing when an empty line is entered"""
+        """Do nothing on empty line"""
         pass
+
+    def do_create(self, arg):
+        """Create a new instance of BaseModel, saves it, and prints the id"""
+        if not arg:
+            print("** class name missing **")
+        elif arg != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            new_instance = BaseModel()
+            new_instance.save()
+            print(new_instance.id)
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
