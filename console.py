43d993be8d8e23c46b87ca2eb7eb2a4e0eecfed3
azizios011 <hdriaziz699@gmail.com>
2023-07-25 06:20:42 -1100

    hzkd

--- console.py
+++ console.py
@@ -7,7 +7,8 @@ from models.user import User
 from models.place import Place
 from models.state import State
 from models.city import City
-
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
