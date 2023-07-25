cce5736c4adc94622e81bce5bd3f8642df7833eb
azizios011 <hdriaziz699@gmail.com>
2023-07-25 04:08:25 -1100

    h

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 # models/__init__.py
+from models.engine.file_storage import FileStorage
 
-# This file is intentionally left empty.
-# It serves as an indicator that the 'models' directory is a package.
-# Any initialization code or imports can be added here if needed.
-# For this specific project, no extra initialization is required in this file.
+storage = FileStorage()
+storage.reload()
+
--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,26 @@
 # models/base_model.py
 import uuid
 from datetime import datetime
+from models import storage
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
-            # If kwargs is not empty, re-create the instance from the dictionary representation
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key != '__class__':
+                if key != "__class__":
                     setattr(self, key, value)
+            self.created_at = datetime.strptime(self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
+            self.updated_at = datetime.strptime(self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
         else:
-            # If kwargs is empty, create a new instance
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.created_at = self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
