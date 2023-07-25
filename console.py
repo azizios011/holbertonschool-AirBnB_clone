9728ba8a4e9d04db9804ea24d71a4d064fb2b87c
azizios011 <hdriaziz699@gmail.com>
2023-07-25 04:26:25 -1100

    Console 0.0.1

--- console.py
+++ console.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import cmd
+import models
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
-
+    
     def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
@@ -12,6 +13,10 @@ class HBNBCommand(cmd.Cmd):
         """Exit the program on EOF (Ctrl + D)"""
         print()
         return True
+
+    def emptyline(self):
+        """Override emptyline method to do nothing when an empty line is entered"""
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
