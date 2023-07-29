7b2b916c190de9843da95fa42973e7399efd7dbc
azizios011 <hdriaziz699@gmail.com>
2023-07-29 03:09:43 -1100

    he(

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+""" the file_storage"""
 import json
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
 from models.place import Place
-
 
 
 class FileStorage:
